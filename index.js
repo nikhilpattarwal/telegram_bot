996aaf4ae30a6e7903686d54221828e789ba0a07
Monust0036 <monusveltosest0036@gmail.com>
2024-09-08 16:41:05 +0530

    port updated

--- index.js
+++ index.js
@@ -147,7 +147,7 @@ bot.command('balance', async (ctx) => {
 
 const PORT = process.env.PORT || 3001;
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log('Server is running on port 3000');
 });
 
